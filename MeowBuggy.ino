5eaf2c4cee643429079e2a472a1c94f3c3828275
azuxul <contact@azuxul.fr>
2016-03-30 12:20:57 +0200

    Add servo motor for ultrasonic sensor

--- MeowBuggy.ino
+++ MeowBuggy.ino
@@ -1,17 +1,49 @@
 #include "Motors.h"
+#include <Servo.h>
+
+Servo servo;
+int motorLoc;
+bool motorNeg;
 
 void setup() {
 
-  pinMode(MOTOR_1_L,OUTPUT);
-  pinMode(MOTOR_1_R,OUTPUT);
-  pinMode(MOTOR_2_L,OUTPUT);
-  pinMode(MOTOR_2_R,OUTPUT);
-  pinMode(MOTOR_3_L,OUTPUT);
-  pinMode(MOTOR_3_R,OUTPUT);
-  pinMode(MOTOR_4_L,OUTPUT);
-  pinMode(MOTOR_4_R,OUTPUT);
+  // Init motors
+  pinMode(MOTOR_1_F,OUTPUT);
+  pinMode(MOTOR_1_B,OUTPUT);
+  pinMode(MOTOR_2_F,OUTPUT);
+  pinMode(MOTOR_2_B,OUTPUT);
+  pinMode(MOTOR_3_F,OUTPUT);
+  pinMode(MOTOR_3_B,OUTPUT);
+  pinMode(MOTOR_4_F,OUTPUT);
+  pinMode(MOTOR_4_B,OUTPUT);
+
+  Serial.begin(9600);
+
+  servo.attach(2);
+  servo.write(0);
+
+  motorLoc = 180;
 }
 
 void loop() {
-  
+
+  delay(10);
+
+  updateMotor();
 }
+
+void updateMotor() {
+  
+  if(motorNeg) {
+    motorLoc-=2;
+  } else {
+    motorLoc+=2;
+  }
+
+  if(motorLoc <= 0 || motorLoc > 180) {
+    motorNeg = !motorNeg;
+  }
+
+  servo.write(motorLoc);
+}
+
